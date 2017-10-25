624b8056e30abc7dfac8626498c4f1304c159905
Gabriel Francisco <gabrinelson27@gmail.com>
2017-10-25 01:04:51 -0200

    Concertano coisas

--- generateQRCode.php
+++ generateQRCode.php
@@ -1,6 +1,6 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "", "bancointer");
-print_r($_POST);
+
 if (!empty($_POST['value']) && !empty($_POST['cnpj'] && !empty($_POST['pin']))){
 	$pin = md5($_POST['pin']);
 	$query = mysqli_query($conn, "SELECT * FROM empresa WHERE cnpj='{$_POST['cnpj']}' AND pin='{$pin}'");
@@ -10,7 +10,6 @@ if (!empty($_POST['value']) && !empty($_POST['cnpj'] && !empty($_POST['pin']))){
 		$token = md5($_POST['value'].$_POST['cnpj'].rand(10000, 99999));
 		mysqli_query($conn, "INSERT INTO qrcodes VALUES(default, {$_POST['value']}, '$token', default, $empresa_id)");
 		echo $token;
-		
 		http_response_code(200);
 	}
 }
